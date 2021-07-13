23fcda109e24216000fc88e652b39d46951bc723
cafeway <bcaranja@gmai.com>
2021-07-13 16:16:47 -0400

    ...

--- src/components/Cards/CardSettings.vue
+++ src/components/Cards/CardSettings.vue
@@ -409,7 +409,7 @@ export default {
     getlink: function () {
       var urlgenerator = require("urlgenerator");
       var createURLwithParameters = urlgenerator.createURLwithParameters;
-      var baseURL = "https://promax.netlify.app/auth/register";
+      var baseURL = "https://promaxcash.com/register";
       var referee = firebase.auth().currentUser.uid;
       var parameters = { uid: referee };
       var finalURL = createURLwithParameters(baseURL, parameters);
